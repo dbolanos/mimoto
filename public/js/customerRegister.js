66180d7352ee04612a3a9f543711ba45b6ea9fa2
dyl-XD@hotmail.com <dyl-XD@hotmail.com>
2018-09-16 07:28:50 -0600

    Improved customerRegister.js

--- public/js/customerRegister.js
+++ public/js/customerRegister.js
@@ -1,57 +1,30 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-$('#province').on('click', function () {
-    console.log('Actualizando Direccion Canton y Distrito...');
-    var province    = $('#province').val();
-    console.log(province);
+    $('#province').on('click', function () {
+        data = {};
+        console.log('Checking Canton and District...');
+        data.province_id = $('#province').val();
 
-    $.ajax({
-        //Como se ejecuta un POST necesitamos el token de CSRF, (lo tomamos de un meta que esta en el blade)
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-             },
-             // En data puedes utilizar un objeto JSON, un array o un query string
-             data: {"province_id" : province},
-             //Cambiar a type: POST si necesario
-             type: "GET",
-             // Formato de datos que se espera en la respuesta
-             dataType: "json",
-             // URL a la que se enviará la solicitud Ajax
-             url: "get-cantons-districts",
-    })
-        .done(function( data, textStatus, jqXHR ) {
-            console.log('Funciona 1');
-            var $select_canton = $("#canton");
-            $select_canton.empty(); // remove old options
-            $.each(data['cantons'], function(key,canton) {
-                $select_canton.append($("<option></option>").attr("value", canton['id']).text(canton['name']));
-            });
-            var $select_district = $("#district");
-            $select_district.empty(); // remove old options
-            $.each(data['districts'], function(key,district) {
-                $select_district.append($("<option></option>").attr("value", district['id']).text(district['name']));
-            });
-        })
-        .fail(function( jqXHR, textStatus, errorThrown ) {
-            console.log('NOOO Funciona');
-        });
-});
-
+        updateZone(data);
+    });
 
     $('#canton').on('click', function () {
-        console.log('Actualizando Direccion Distrito...');
+        data = {};
+        console.log('Checking Districts...');
+        data.canton_id = $('#canton').val();
 
-        var province             = $('#province').val();
-        var canton               = $('#canton').val();
-        console.log(province);
+        updateZone(data);
+    });
 
+    function updateZone(data) {
+        console.log(data);
         $.ajax({
             //Como se ejecuta un POST necesitamos el token de CSRF, (lo tomamos de un meta que esta en el blade)
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             },
             // En data puedes utilizar un objeto JSON, un array o un query string
-            data: {"province_id" : province, "canton_id": canton},
+            data: data,
             //Cambiar a type: POST si necesario
             type: "GET",
             // Formato de datos que se espera en la respuesta
@@ -59,18 +32,28 @@ $('#province').on('click', function () {
             // URL a la que se enviará la solicitud Ajax
             url: "get-cantons-districts",
         })
-            .done(function( data, textStatus, jqXHR ) {
-                console.log('Funciona 2');
-                var $select_district = $("#district");
-                $select_district.empty(); // remove old options
-                $.each(data['districts'], function(key,district) {
-                    $select_district.append($("<option></option>")
-                        .attr("value", district['id']).text(district['name']));
-                });
+            .done(function (result, textStatus, jqXHR) {
+                console.log('Update...');
+                if (result['cantons']) {
+                    var $select_canton = $("#canton");
+                    $select_canton.empty(); // remove old options
+                    $.each(result['cantons'], function (key, canton) {
+                        $select_canton.append($("<option></option>").attr("value", canton['id']).text(canton['name']));
+                    });
+                }
+
+                if (result['districts']) {
+                    var $select_district = $("#district");
+                    $select_district.empty(); // remove old options
+                    $.each(result['districts'], function (key, district) {
+                        $select_district.append($("<option></option>").attr("value", district['id']).text(district['name']));
+                    });
+                }
+
             })
-            .fail(function( jqXHR, textStatus, errorThrown ) {
-                console.log('NOOO Funciona');
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                console.log('Request Fail...');
             });
-    });
+    }
 
 });
