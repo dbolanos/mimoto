6f69ba7d8fdee81eaee131277914c0f3cf309d55
dbolanos <dbolanos@mybookie.cr>
2018-09-21 14:33:10 -0600

    Fix data-base migrate issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
+        date_default_timezone_set('America/Costa_Rica');
     }
 
     /**
