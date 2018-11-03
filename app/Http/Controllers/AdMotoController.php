f5c7ef9eed7e7208a10a40afcb8adc9f3b12abc5
dyl-XD@hotmail.com <dyl-XD@hotmail.com>
2018-11-03 00:20:15 -0600

    Add ExtraMoto and change in anunciese aqui

--- app/Http/Controllers/AdMotoController.php
+++ app/Http/Controllers/AdMotoController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ExtraMoto;
 use Illuminate\Http\Request;
 use App\Models\MotorcycleBrand;
+use App\Models\TypeMotorcycle;
 
 class AdMotoController extends Controller
 {
@@ -12,7 +14,10 @@ class AdMotoController extends Controller
     public function getCreateAdMoto(){
 
         $motorcycle_brands  = MotorcycleBrand::where('status', true)->orderby('description')->get();
+        $type_motorcycles   = TypeMotorcycle::where('status', true)->orderby('description')->get();
 
-        return view('ad_moto.create_ad_moto', compact('motorcycle_brands', 'type_motorcycles'));
+        $extras_moto = ExtraMoto::where('status', true)->orderby('description')->get();
+
+        return view('ad_moto.create_ad_moto', compact('motorcycle_brands', 'type_motorcycles', 'extras_moto'));
     }
 }
--- resources/views/ad_moto/create_ad_moto.blade.php
+++ resources/views/ad_moto/create_ad_moto.blade.php
@@ -27,6 +27,15 @@
         </div>
 
         <div class="form-group">
+            <label for="moto-category">Categoría:</label>
+            <select class="combo-mimoto" id="MotoBrand">
+                @foreach ($type_motorcycles as $type_motorcycles)
+                    <option value="{{$type_motorcycles->id}}"> {{$type_motorcycles->description}} </option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="form-group">
             <label for="moto-brand">Modelo:</label>
             <select class="combo-mimoto" id="moto-brand">
                 <option value="0"> Año </option>
@@ -40,6 +49,34 @@
             <label for="moto-mileage">Kilometraje</label>
             <input id="moto-mileage" type="text" class="form-control">
         </div>
+
+        <div class="form-group">
+            <label for="moto-displacement">Cilindrada</label>
+            <input id="moto-displacement" type="text" class="form-control">
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">Extras Moto</div>
+            <div class="panel-body">
+                @foreach($extras_moto as $extra_moto)
+                    {{--<div class="form-group">--}}
+                        {{--<label for="moto-extra-{{$extra_moto->id}}" class="checkbox-inline">{{$extra_moto->description}}--}}
+                            {{--<input id="moto-extra-{{$extra_moto->id}}" type="checkbox" class="form-control">--}}
+                        {{--</label>--}}
+                    {{--</div>--}}
+                    {{--<div class="checkbox" style="display:inline">--}}
+                        {{--<label><input  id="moto-extra-{{$extra_moto->id}}" type="checkbox" class="form-control">{{$extra_moto->description}}</label>--}}
+                    {{--</div>--}}
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="moto-extra-{{$extra_moto->id}}">
+                        <label class="custom-control-label" for="moto-extra-{{$extra_moto->id}}">{{$extra_moto->description}}</label>
+                    </div>
+                @endforeach
+            </div>
+
+        </div>
+
+
 
 
 
